46ea31edb4ad92f5bcc669cd402ea96db1407e8d
TiernanH <TiernanH@users.noreply.github.com>
2020-11-29 12:58:06 +0000

    alpha update

---
+++ College Game Project/Assets/Scripts/TrapReset.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrapReset : MonoBehaviour
+{
+    [SerializeField] private string loadLevel;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(loadLevel);
+        }
+    }
+}
+
